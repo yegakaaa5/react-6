b08384452e83d6b96ddd40de786294d9cb148180
yegakaaa5 <yegakaaa5@gmail.com>
2024-10-19 16:19:51 +0330

    adding code

---
+++ src/component/card/card1.js
@@ -0,0 +1,17 @@
+import "../card/card1.css";
+
+function card1() {
+    return (
+        <div class="card" style="width: 18rem;">
+            <img src="./assets/tone mahi.webp" className="card-img-top" alt="تن ماهی" />
+            <div class="card-body">
+                <h5 class="card-title">تومان ۷۰,۰۰۰
+                </h5>
+                <p class="card-text">کنسرو ماهی تن در روغن گیاهی پیک - 180 گرم</p>
+                <a href="#" class="btn btn-primary">افزودن به سبد خرید</a>
+            </div>
+        </div>
+    )
+}
+card1()
+export default card1;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import './component/card/card1.css';
+import '../src/card2.css';
+import '../src/card3.css';
+import '../src/card4.css';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+//card2
+const card2 = ReactDOM.createRoot(document.getElementById('card2'));
+card2.render(
   <React.StrictMode>
-    <App />
+    <div class="card-body">
+      <img class="card-img" src="./assets/all in one.webp" alt="all in one"/>
+        <h4 class="card-details">کامپیوتر همه کاره 23.8 اینچ اچ‌ پی مدل 24-CR0123-i5 1335U 8GB 512SSD UHD</h4><br/>
+        <h3 class="card-price"> تومان ۴۶,۵۰۰,۰۰۰
+        </h3>
+        <button>افزودن به سبد خرید</button>
+    </div>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+//card3
+const card3 = ReactDOM.createRoot(document.getElementById('card3'));
+card3.render(
+  React.createElement()
+);
+
